07b94ea497326d9579952c202fc732c4ff92fc94
Matthew Stephens <mstephens@decisiohealth.com>
2023-08-29 18:12:17 -0500

    crossing fingers

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<!-- src/routes/layout.svelte -->
+
+<script>
+    // Import any dependencies or logic needed for the layout
+    
+  </script>
+  
+  <header>
+    <hr>
+  </header>
+  
+  <main>
+    <slot /> <!-- This slot will be replaced with the content of individual pages -->
+  </main>
+  
+  <footer>
+    <hr>
+  </footer>
+  
+  <style>
+    /* Define your layout styles here */
+  </style>
+  
